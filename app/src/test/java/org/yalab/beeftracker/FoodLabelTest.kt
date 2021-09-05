ab9fd608c398c425c9bf935b9c3d3d5720bcae39
yalab <rudeboyjet@gmail.com>
2021-09-05 17:42:13 +0900

    Use Robolectric

--- app/src/test/java/org/yalab/beeftracker/FoodLabelTest.kt
+++ app/src/test/java/org/yalab/beeftracker/FoodLabelTest.kt
@@ -1,10 +1,13 @@
 package org.yalab.beeftracker
 
 import org.junit.Before
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
 import org.junit.Assert.*
 import org.junit.Test
 
+@RunWith(RobolectricTestRunner::class)
 class FoodLabelTest {
     lateinit var foodLabel : FoodLabel
     @Before
