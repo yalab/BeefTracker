f56feabac97cb351506ae88007b01853e1e357d1
yalab <rudeboyjet@gmail.com>
2021-09-05 17:00:56 +0900

    Move traindata.

--- app/src/main/java/org/yalab/beeftracker/FoodLabel.kt
+++ app/src/main/java/org/yalab/beeftracker/FoodLabel.kt
@@ -1,7 +1,9 @@
 package org.yalab.beeftracker
 
 class FoodLabel {
+    val TRAIND_DATA_PATH = "eng.traineddata"
     fun detect() : String {
+        val path = this.javaClass.classLoader.getResource(TRAIND_DATA_PATH)
         return "1234"
     }
 }
