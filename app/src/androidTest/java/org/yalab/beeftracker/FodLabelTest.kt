73e7801e71ebb5fab754bd1a5b3e7a9bcf09771f
yalab <rudeboyjet@gmail.com>
2021-09-07 23:44:55 +0900

    TessTwo works fine but its not so good.

---
+++ app/src/androidTest/java/org/yalab/beeftracker/FodLabelTest.kt
@@ -0,0 +1,28 @@
+package org.yalab.beeftracker
+
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+
+@RunWith(AndroidJUnit4::class)
+class FodLabelTest {
+    lateinit var foodLabel : FoodLabel
+    lateinit var context : Context
+    @Before
+    fun setUp() {
+        foodLabel = FoodLabel()
+        context = InstrumentationRegistry.getInstrumentation().targetContext
+    }
+
+    @Test
+    fun foodLabelDetect() {
+        val image = context.assets.open("image.jpg")
+        foodLabel.detect(context, image)
+    }
+}
--- app/src/main/java/org/yalab/beeftracker/FoodLabel.kt
+++ app/src/main/java/org/yalab/beeftracker/FoodLabel.kt
@@ -1,9 +1,32 @@
 package org.yalab.beeftracker
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import com.googlecode.tesseract.android.TessBaseAPI
+import java.io.File
+import java.io.InputStream
+
 class FoodLabel {
-    val TRAIND_DATA_PATH = "eng.traineddata"
-    fun detect() : String {
-        val path = this.javaClass.classLoader.getResource(TRAIND_DATA_PATH)
-        return "1234"
+    fun detect(context: Context, inputStream: InputStream) : String {
+
+        val dir = File(context.filesDir, "tessdata")
+//        if(!dir.exists()){
+            dir.mkdir()
+            val filename = "tessdata/jpn.traineddata"
+            val src = context.assets.open(filename)
+            val dst = File(dir.toString() + "/jpn.traineddata")
+            dst.createNewFile()
+            dst.outputStream().use {
+                it.write(src.readBytes())
+            }
+//        }
+
+        val baseApi = TessBaseAPI()
+        baseApi.init(context.filesDir.toString(), "jpn")
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        baseApi.setImage(bitmap)
+        val recognizedText = baseApi.utF8Text
+        baseApi.end()
+        return recognizedText
     }
 }
