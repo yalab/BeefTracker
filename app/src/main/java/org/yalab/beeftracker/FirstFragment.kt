e174755c038f2459f869afc6852ca61982c70cac
yalab <rudeboyjet@gmail.com>
2021-09-08 23:55:37 +0900

    First use opencv.

--- app/src/main/java/org/yalab/beeftracker/FirstFragment.kt
+++ app/src/main/java/org/yalab/beeftracker/FirstFragment.kt
@@ -1,12 +1,19 @@
 package org.yalab.beeftracker
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import org.opencv.android.LoaderCallbackInterface
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
 import org.yalab.beeftracker.databinding.FragmentFirstBinding
 
 /**
@@ -37,6 +44,18 @@ class FirstFragment : Fragment() {
             val context = context as Context
             val image = context.assets.open("image.png")
             binding.textviewFirst.text = foodLabel.detect(image)
+            val image2 = context.assets.open("image.jpg")
+//            val bitmap = foodLabel.preProcess(image)
+
+
+            var bmp = BitmapFactory.decodeStream(image2)
+            OpenCVLoader.initDebug();
+            var mat = Mat()
+            Utils.bitmapToMat(bmp, mat)
+            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY)
+            var output = Bitmap.createBitmap(bmp.width, bmp.height, Bitmap.Config.ARGB_8888)
+            Utils.matToBitmap(mat, output)
+            binding.imageView.setImageBitmap(output)
         }
     }
 
