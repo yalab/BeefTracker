964f4220d697df226b746976eba6927e3fada5ec
yalab <rudeboyjet@gmail.com>
2021-09-10 23:21:28 +0900

    FoodLabel#detect(Bimap)

--- app/src/main/java/org/yalab/beeftracker/FoodLabel.kt
+++ app/src/main/java/org/yalab/beeftracker/FoodLabel.kt
@@ -1,6 +1,7 @@
 package org.yalab.beeftracker
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import com.googlecode.tesseract.android.TessBaseAPI
 import java.io.File
@@ -20,10 +21,15 @@ class FoodLabel constructor(_context: Context) {
             dataFile.outputStream().use { it.write(src.readBytes()) }
         }
     }
+
     fun detect(inputStream: InputStream) : String {
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        return detect(bitmap)
+    }
+
+    fun detect(bitmap: Bitmap) : String {
         val baseApi = TessBaseAPI()
         baseApi.init(context.filesDir.toString(), "eng")
-        val bitmap = BitmapFactory.decodeStream(inputStream)
         baseApi.setImage(bitmap)
         val recognizedText = baseApi.utF8Text as String
         baseApi.end()
