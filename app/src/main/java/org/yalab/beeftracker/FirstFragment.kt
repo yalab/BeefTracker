dec8aafb2d9a86b2332a43ad2642684ec46b6466
yalab <rudeboyjet@gmail.com>
2021-09-11 16:09:20 +0900

    First to use Imgproc.drawContours.

--- app/src/main/java/org/yalab/beeftracker/FirstFragment.kt
+++ app/src/main/java/org/yalab/beeftracker/FirstFragment.kt
@@ -12,7 +12,6 @@ import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
 import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
-import org.opencv.imgproc.Imgproc.circle
 import org.yalab.beeftracker.databinding.FragmentFirstBinding
 
 
@@ -22,6 +21,7 @@ import org.yalab.beeftracker.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
     private lateinit var foodLabel : FoodLabel
+    var step = 0
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -52,11 +52,39 @@ class FirstFragment : Fragment() {
             var src = Mat()
             val image2 = context.assets.open("image.jpg")
             Utils.bitmapToMat(BitmapFactory.decodeStream(image2), src)
-
             val dst = Mat.zeros(Size(src.width().toDouble(), src.height().toDouble()), CvType.CV_8UC3)
-            Imgproc.cvtColor(src, dst, Imgproc.COLOR_RGB2GRAY)
-            Imgproc.Canny(dst, dst, 50.0, 200.0);
+            src.copyTo(dst)
+            if(step >= 0) { Imgproc.cvtColor(dst, dst, Imgproc.COLOR_RGB2GRAY) }
+            if(step >= 1) { Imgproc.GaussianBlur(dst, dst, Size(5.0, 5.0), 5.0) }
+            if(step >= 2) { Imgproc.Sobel(dst, dst, -1, 0, 1) }
+            if(step >= 3) { Imgproc.threshold(dst, dst, 100.0, 255.0, Imgproc.THRESH_BINARY)}
+            if(step >= 4) { Imgproc.morphologyEx(dst, dst, Imgproc.MORPH_CLOSE, Mat(3, 3, CvType.CV_8UC1), Point(-1.0, -1.0), 3)}
+            if(step >= 5) {
+                val hierarchy = Mat()
+                val contours = ArrayList<MatOfPoint>()
+                Imgproc.findContours(
+                    dst,
+                    contours,
+                    hierarchy,
+                    Imgproc.RETR_LIST,
+                    Imgproc.CHAIN_APPROX_TC89_L1
+                )
+                src.copyTo(dst)
+                Imgproc.drawContours(dst, contours, -1, Scalar(255.0, 0.0, 0.0), 10)
 
+                val blue = Scalar(255.0, 0.0, 0.0)
+                val green = Scalar(0.0, 255.0, 0.0)
+                contours.forEach({ ptmat ->
+                    val ptmat2 = MatOfPoint2f(*ptmat.toArray())
+                    val bbox = Imgproc.minAreaRect(ptmat2)
+                    val box = bbox.boundingRect()
+                    Imgproc.circle(dst, bbox.center, 5, blue, -1)
+                    Imgproc.rectangle(dst, box.tl(), box.br(), green, 10)
+                })
+            }
+            if(step >= 6) { src.copyTo(dst); step = -1 }
+            step ++
+            binding.buttonFirst.text = step.toString()
             var output = Bitmap.createBitmap(dst.width(), dst.height(), Bitmap.Config.ARGB_8888)
             Utils.matToBitmap(dst, output)
             binding.imageView.setImageBitmap(output)
