a45e1be33776b20ec3112fd2787fba855d5102c8
yalab <rudeboyjet@gmail.com>
2021-10-18 23:58:33 +0900

    Followed FoodLabel#texts privately to pass all tests in Local.

--- app/src/androidTest/java/org/yalab/beeftracker/FoodLabelTest.kt
+++ app/src/androidTest/java/org/yalab/beeftracker/FoodLabelTest.kt
@@ -14,15 +14,17 @@ import kotlin.reflect.jvm.isAccessible
 @RunWith(AndroidJUnit4::class)
 class FoodLabelTest {
     lateinit var context : Context
-    lateinit var foodLabel : FoodLabel
     @Before
     fun setUp() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
-        foodLabel = FoodLabel(context)
     }
 
-    fun foodLabel(filename: String) : FoodLabel {
+    fun foodLabel(filename: String): FoodLabel {
         return FoodLabel(context, context.assets.open(filename))
+    }
+
+    fun foodLabel(): FoodLabel {
+        return FoodLabel(context)
     }
 
     @Test
@@ -30,36 +32,27 @@ class FoodLabelTest {
         assertEquals("org.yalab.beeftracker", context.packageName)
     }
 
-//    @Test
-//    fun foodLabelPNG() {
-//        assertEquals(listOf("173456", "890 \\"), foodLabel("image.png").texts)
-//    }
-//
-//    @Test
-//    fun foodLabelJPG() {
-//        assertEquals(listOf("|l ,_\nflﬁiﬁﬁﬂ'lﬁﬁ\n1490915461"), foodLabel("image.jpg").texts)
-//    }
-//
-//    @Test
-//    fun beefTrackingNumberPNG() {
-//        assertEquals("", foodLabel("image.png").beefTrackingNumber())
-//    }
-//
-//    @Test
-//    fun beefTrackingNumberJPG() {
-//        assertEquals("1490915461", foodLabel("image.jpg").beefTrackingNumber())
-//    }
-//
-//    @Test
-//    fun addBeefTrackingNumber() {
-//        val beefTrackingNumbers: List<String> = listOf("1466716191", "1466716191")
-//        val method = foodLabel::class.members.find{ it.name == "addBeefTrackingNumbers" }?.apply{ isAccessible = true }
-//        method?.call(foodLabel, beefTrackingNumbers)
-//        assertEquals(listOf("1466716191") as List<String>, foodLabel.beefTrackingNumbers)
-//    }
-//
-//    @Test
-//    fun emptyBeefTrackingNumber() {
-//        assertEquals("", FoodLabel(context).beefTrackingNumber())
-//    }
+    @Test
+    fun beefTrackingNumberPNG() {
+        assertEquals("", foodLabel("image.png").beefTrackingNumber())
+    }
+
+    @Test
+    fun beefTrackingNumberJPG() {
+        assertEquals("1490915461", foodLabel("image.jpg").beefTrackingNumber())
+    }
+
+    @Test
+    fun addBeefTrackingNumber() {
+        val beefTrackingNumbers: List<String> = listOf("1466716191", "1466716191")
+        val foodLabel = foodLabel()
+        val method = foodLabel::class.members.find{ it.name == "addBeefTrackingNumbers" }?.apply{ isAccessible = true }
+        method?.call(foodLabel, beefTrackingNumbers)
+        assertEquals(listOf("1466716191") as List<String>, foodLabel.beefTrackingNumbers)
+    }
+
+    @Test
+    fun emptyBeefTrackingNumber() {
+        assertEquals("", FoodLabel(context).beefTrackingNumber())
+    }
 }
